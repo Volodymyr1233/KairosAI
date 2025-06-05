413ee8dfdd4d2af0f95eeae88dcd7ab7d2639d9b
Slawomir <slawek.salamon44@gmail.com>
2025-06-05 23:14:11 +0200

    working get credentials etc

--- Credentials/CredentialsFunctions.py
+++ Credentials/CredentialsFunctions.py
@@ -1,4 +1,6 @@
-import _local_credentials
+from Credentials._local_resources import _local_credentials
+
+
 def create_authorization_url(user_id:str)->str:
     """:returns authorization url"""
     _local_credentials.auth_into_db(user_id)
@@ -12,9 +14,12 @@ def get_user_credential(user_id:str)->dict | None:
     return _local_credentials.get_credentials_from_db(user_id)
 
 if __name__ == '__main__':
-
-    create_authorization_url('6')
+    from GoogleAPI import GoogleCalendarAPI
+    #create_authorization_url('6')
     x = get_user_credential('6')
     if check_user_credentials('6'):
         for k,e in x.items():
             print(f'{k}: {e}')
+    e = GoogleCalendarAPI.getEvents(x)
+    for event in e:
+        print(event)
